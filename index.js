e1760e38c85ac6d935bfbb735307806b3fed0342
Paulo Ferrari <paulo.ferrari@involves.com>
2021-09-03 15:46:35 -0300

    export nats string codec

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const NatsStreamConnection = require('./src/nats-stream-connection')
 const NatsStreamMiddleware = require('./src/nats-stream-middleware')
 const NatsStreamService = require('./src/nats-stream-service')
+const { StringCodec } = require('nats')
 
 module.exports = {
   NatsStreamConnection,
   NatsStreamMiddleware,
-  NatsStreamService
+  NatsStreamService,
+  StringCodec
 }
